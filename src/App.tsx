acb0750b700c59e1f1a91644a4707802096c21e5
John Robinson <john.robinson@rev.io>
2025-02-15 12:56:55 -0600

    Expand form example

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useFormReset } from './use-form-reset';
 import { useFormState } from './use-form-state';
 import { useFormValues } from './use-form-values';
 
-const defaultFormValues = { name: "John" };
+const defaultFormValues = { name: "John", phone: "555-5555", email: "john@example.com" };
 
 function App() {
 
@@ -17,6 +17,16 @@ function App() {
     name: (value) => {
       console.log('validating name', value);
       if (!value) return "Name is required";
+      return undefined;
+    },
+    phone: (value) => {
+      console.log('validating phone', value);
+      if (!value) return "Phone is required";
+      return undefined;
+    },
+    email: (value) => {
+      console.log('validating email', value);
+      if (!value) return "Email is required";
       return undefined;
     }
   }})
@@ -39,9 +49,19 @@ function App() {
         <input name="name" disabled={state?.isSubmitted} />
         <p data-validator-for="name" />
       </div>  
+      <div>
+        <label style={{display: "block"}} htmlFor="name">Phone</label>
+        <input name="phone" disabled={state?.isSubmitted} />
+        <p data-validator-for="phone" />
+      </div>  
+      <div>
+        <label style={{display: "block"}} htmlFor="email">Email</label>
+        <input name="email" disabled={state?.isSubmitted} />
+        <p data-validator-for="email" />
+      </div>  
       <button disabled={state?.isSubmitted} type="submit">Submit</button>
     </form>
-    <div style={{ textAlign: "left", color: state?.isSubmitted ? "green": "grey"}}>{values.name} {state?.isSubmitted ? "✅" : ""}</div>
+    <div style={{ textAlign: "left", color: state?.isSubmitted ? "green": "grey"}}>{values.name} {values.phone} { values.email} {state?.isSubmitted ? "✅" : ""}</div>
     <button disabled={!state?.isSubmitted} onClick={() => reset()}>Reset</button>
     </>
   )
