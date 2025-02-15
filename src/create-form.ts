f45bba36948de751f95d480e4ec94f5eaa06fa08
John Robinson <john.robinson@rev.io>
2025-02-15 14:05:20 -0600

    Updated state value setters

--- src/create-form.ts
+++ src/create-form.ts
@@ -12,7 +12,6 @@ export const createForm = <T extends string, TData extends object>(
   formName: T,
   initialValues: TData
 ): ReactiveForm<TData> => {
-
   const createInitialState = (): ReactiveFormState<T, TData> => ({
     formName,
     dirty: {
@@ -60,76 +59,84 @@ export const createForm = <T extends string, TData extends object>(
       },
     });
     window.dispatchEvent(evt);
-  }
+  };
 
   return {
     defaultValue: initialValues,
-    formName,    
+    formName,
     setValues: (values) => {
-       const curr = formDom.read<T, TData>(formName)!;
+      const curr = formDom.read<T, TData>(formName)!;
 
-       formDom.write(formName, {
+      formDom.write(formName, {
         ...curr,
         values: {
-            ...curr.values,
-            values: {
-                ...curr.values.values,
-                ...values
-            }
-        }
-       });
+          ...curr.values,
+          values: {
+            ...curr.values.values,
+            ...values,
+          },
+        },
+      });
 
-       notifyListeners();
+      notifyListeners();
     },
     setErrors: (errors) => {
-        const curr = formDom.read<T, TData>(formName)!;
-        formDom.write(formName, {
-            ...curr,
-            errors: {
-                ...curr.errors,
-                values: errors
-            }
-        });
-        notifyListeners();
+      const curr = formDom.read<T, TData>(formName)!;
+      formDom.write(formName, {
+        ...curr,
+        errors: {
+          name: formName,
+          values: {
+            ...curr.errors.values,
+            ...errors,
+          },
+        },
+      });
+      notifyListeners();
     },
     setDirty: (dirty) => {
-        const curr = formDom.read<T, TData>(formName)!;
-        formDom.write(formName, {
-            ...curr,
-            dirty: {
-                ...curr.dirty,
-                values: dirty
-            },
-            isDirty: true,
-        });
-        notifyListeners();
+      const curr = formDom.read<T, TData>(formName)!;
+      formDom.write(formName, {
+        ...curr,
+        dirty: {
+          name: formName,
+          values: {
+            ...curr.dirty,
+            ...dirty,
+          },
+        },
+        isDirty: true,
+      });
+      notifyListeners();
     },
     setTouched: (touched) => {
-        const curr = formDom.read<T, TData>(formName)!;
-        formDom.write(formName, {
-            ...curr,
-            touched: {
-                ...curr.touched,
-                values: touched
-            },
-            isTouched: true,
-        });
-        notifyListeners();
+      const curr = formDom.read<T, TData>(formName)!;
+      formDom.write(formName, {
+        ...curr,
+        touched: {
+          name: formName,
+          values: {
+            ...curr.touched.values,
+            ...touched,
+          },
+        },
+        isTouched: true,
+      });
+      notifyListeners();
     },
     reset: () => {
-        formDom.write(formName, createInitialState());
-        notifyListeners();
+      formDom.write(formName, createInitialState());
+      notifyListeners();
     },
     submit: () => {
-        const formState = formDom.read<T, TData>(formName)!;
-        formDom.write(formName, {
-            ...formState,
-            isSubmitted: true
-        });
-        notifyListeners(true);
-        
-        return formState.values.values
+      const formState = formDom.read<T, TData>(formName)!;
+      formDom.write(formName, {
+        ...formState,
+        isSubmitted: true,
+      });
+      notifyListeners(true);
+
+      return formState.values.values;
     },
   };
 };
-
